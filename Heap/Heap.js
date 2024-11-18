daba477bbca6ace545447a4ba9d67b4de9e90676
Isaac Lockwood <isaaclockwood618@gmail.com>
2024-11-18 15:35:59 -0600

    Added Removal of Index
    Can remove an index from the table (need to add a visual table that holds the values as well)

--- Heap/Heap.js
+++ Heap/Heap.js
@@ -1,70 +1,102 @@
 // Constructor for Heap
 function Heap() {
-  this.list = [];
+    this.list = [];
 }
-           
+
 Heap.prototype = {
-  // Add a new object into the heap
-  add: function(e) {
-    this.list.push(e); // Append to the heap
-    var currentIndex = this.list.length - 1; // The index of the last node
+    // Add a new object into the heap
+    add: function (e) {
+        this.list.push(e); // Append to the heap
+        var currentIndex = this.list.length - 1; // The index of the last node
+        this.heapifyUp(currentIndex); // Maintain heap property by heapifying up
+    },
 
-    while (currentIndex > 0) {
-      var parentIndex = Math.floor((currentIndex - 1) / 2);
-      
-      // Swap if the current object is greater than its parent
-      if (this.list[currentIndex] > this.list[parentIndex]) {
-        var temp = this.list[currentIndex];
-        this.list[currentIndex] = this.list[parentIndex];
-        this.list[parentIndex] = temp;
-        currentIndex = parentIndex;
-      }
-      else 
-        break; // the tree is a heap now
-    }
-  },
-      
-  // Remove the root from the heap 
-  remove: function() {
-    if (this.list.length == 0) return null;
+    // Remove the root from the heap
+    remove: function () {
+        if (this.list.length == 0) return null;
 
-    var removedObject = this.list[0];
-    this.list[0] = this.list[this.list.length - 1];
-    this.list.pop();
+        var removedObject = this.list[0];
+        this.list[0] = this.list[this.list.length - 1];
+        this.list.pop(); // Remove the last element
+        this.heapifyDown(0); // Restore the heap property from the root
 
-    var currentIndex = 0;
-    while (currentIndex < this.list.length) {
-      var leftChildIndex = 2 * currentIndex + 1;
-      var rightChildIndex = 2 * currentIndex + 2;
+        return removedObject;
+    },
 
-      // Find the maximum between two children
-      if (leftChildIndex >= this.list.length) break; // The tree is a heap
-      var maxIndex = leftChildIndex;
-      if (rightChildIndex < this.list.length) {
-        if (this.list[maxIndex] < this.list[rightChildIndex]) {
-          maxIndex = rightChildIndex;
+    // Delete a specific element at a given index
+    delete: function (index) {
+        if (index < 0 || index >= this.list.length) {
+            return null; // Index out of bounds
         }
-      }
 
-      // Swap if the current node is less than the maximum
-      if (this.list[currentIndex] < this.list[maxIndex]) {
-        var temp = this.list[maxIndex];
-        this.list[maxIndex] = this.list[currentIndex];
-        this.list[currentIndex] = temp;
-        currentIndex = maxIndex;
-      }
-      else
-        break; // The tree is a heap
-    }
+        // Replace the element at the index with the last element
+        var removedObject = this.list[index];
+        this.list[index] = this.list[this.list.length - 1];
+        this.list.pop(); // Remove the last element
 
-    return removedObject;
-  },
+        // Restore the heap property
+        var parentIndex = Math.floor((index - 1) / 2);
 
-  getSize: function() {
-    return this.list.length;
-  },
-                  
-  isEmpty: function() {
-    return this.list.length == 0;
-  }
-}
+        // Heapify up if necessary
+        if (index > 0 && this.list[index] > this.list[parentIndex]) {
+            this.heapifyUp(index);
+        } else {
+            // Heapify down if necessary
+            this.heapifyDown(index);
+        }
+
+        return removedObject;
+    },
+
+    // Heapify up method to maintain heap property after insertion or deletion
+    heapifyUp: function (index) {
+        while (index > 0) {
+            var parentIndex = Math.floor((index - 1) / 2);
+            if (this.list[index] > this.list[parentIndex]) {
+                // Swap
+                var temp = this.list[index];
+                this.list[index] = this.list[parentIndex];
+                this.list[parentIndex] = temp;
+                index = parentIndex;
+            } else {
+                break; // Heap property is satisfied
+            }
+        }
+    },
+
+    // Heapify down method to maintain heap property after deletion or root removal
+    heapifyDown: function (index) {
+        while (index < this.list.length) {
+            var leftChildIndex = 2 * index + 1;
+            var rightChildIndex = 2 * index + 2;
+
+            // Find the maximum between two children
+            if (leftChildIndex >= this.list.length) break; // No children, stop
+            var maxIndex = leftChildIndex;
+            if (rightChildIndex < this.list.length) {
+                if (this.list[maxIndex] < this.list[rightChildIndex]) {
+                    maxIndex = rightChildIndex;
+                }
+            }
+
+            // Swap if the current node is less than the maximum child
+            if (this.list[index] < this.list[maxIndex]) {
+                var temp = this.list[maxIndex];
+                this.list[maxIndex] = this.list[index];
+                this.list[index] = temp;
+                index = maxIndex;
+            } else {
+                break; // Heap property is satisfied
+            }
+        }
+    },
+
+    // Utility methods
+    getSize: function () {
+        return this.list.length;
+    },
+
+    isEmpty: function () {
+        return this.list.length == 0;
+    },
+};
